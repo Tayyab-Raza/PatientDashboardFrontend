dc5e53a2b8a6324f3fd14f2b51be49204b681a0e
Tayyab-Raza <traza9192@gmail.com>
2025-05-13 10:59:20 +0530

    latest push

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
